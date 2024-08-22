9a77ea8dab45452abb5e956f28a1d0831a15e1e5
Prabhat Agarwal <prabhat.agarwal0605@gmail.com>
2024-08-22 22:22:47 +0530

    creating client using realm file

--- Identity_Federation/docker_setup/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Wait for Keycloak to be fully up
-sleep 30
-
-chmod +x /opt/keycloak/entrypoint.sh
-
-# Now start Keycloak with import
-/opt/keycloak/bin/kc.sh start-dev --import-realm
