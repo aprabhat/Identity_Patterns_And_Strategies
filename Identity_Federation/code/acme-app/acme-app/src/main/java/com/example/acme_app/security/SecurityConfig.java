bf6b98797094605025fba85f93bf460250669e87
Prabhat Agarwal <prabhat.agarwal0605@gmail.com>
2024-08-27 21:41:50 +0530

    added security file

--- Identity_Federation/code/acme-app/acme-app/src/main/java/com/example/acme_app/security/SecurityConfig.java
+++ Identity_Federation/code/acme-app/acme-app/src/main/java/com/example/acme_app/security/SecurityConfig.java
@@ -9,11 +9,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
+        http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().oauth2Login();
     }
 }
